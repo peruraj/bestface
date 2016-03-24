864ce0c5f34d5ee6ce66408a9d5978f8c9053890
peruraj <perusr@gmail.com>
2016-03-24 17:44:42 +0530

    update

---
+++ trial.py
@@ -0,0 +1,33 @@
+import cv2
+import sys
+import os
+import numpy as numpy
+#face=cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+face=cv2.CascadeClassifier('haarcascade_frontalface_alt.xml')
+eye=cv2.CascadeClassifier('haarcascade_eye.xml')
+cap=cv2.VideoCapture(0)
+i=0;
+while True:
+	ret,img= cap.read()
+	gray=cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+	faces=face.detectMultiScale(gray,1.3,5)
+        
+	for(x,y,w,h) in faces :
+                cv2.rectangle(img,(x,y),(x+w,y+h),(255,0,0),2)
+                roi_gray=gray[y:y+h, x:x+w]
+                roi_color=img[y:y+h, x:x+w]
+                eyes=eye.detectMultiScale(roi_gray)
+                for (ex,ey,ew,eh) in eyes:
+                        i=i+1
+                       # cv2.rectangle(roi_color,(ex,ey),(ex+ew,ey+eh),(255,255,0),2)
+                      	filename = os.path.join('./', 'image' + '%03d.jpg' % i)
+                	cv2.imwrite(filename, roi_color)
+                	if i==5:
+                               sys.exit()
+				
+	cv2.imshow('vid',img)
+	k=cv2.waitKey(30) & 0xff
+	if k==27:
+		break
+cap.release()
+cv2.destroyAllWindows()
